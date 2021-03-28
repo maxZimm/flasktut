0536a693e36c41ceedea7d905103c75ae2b2110f
Max Zimmermann <mkz.87.draw@gmail.com>
2021-03-27 20:44:02 -0700

    added allembic ran first migration

--- alembic/env.py
+++ alembic/env.py
@@ -4,6 +4,13 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from os import environ, path
+from dotenv import load_dotenv
+from models.base import SqlAlchemyBase
+import models.__all_models
+
+basedir = path.abspath(path.dirname('..'))
+load_dotenv(path.join(basedir,'.env'))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,13 +24,13 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SqlAlchemyBase.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
+config.set_main_option('sqlalchemy.url', environ.get('DATABASE_URL'))
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
