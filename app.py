12e15e7482863ae9e9ac92387a006beabb60c996
Max Zimmermann <mkz.87.draw@gmail.com>
2021-03-27 19:30:53 -0700

    add alembic dotenv and setup config file for ability to have multi-env setup

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import flask
 import db_session
+import os
 
 app = flask.Flask(__name__)
+app.config.from_object(os.environ.get('APP_SETTINGS'))
 
 def main():
     register_blueprints()
-    db_session.global_init('postgres://localhost/plannit')
+    db_session.global_init(app.config['SQLALCHEMY_DATABASE_URI'])
     app.run(debug = True)
 
 def register_blueprints(app=app):
@@ -19,4 +21,4 @@ if __name__ == '__main__':
     main() 
 else:
     register_blueprints()
-    db_session.global_init('postgres://localhost/plannit')
+    db_session.global_init(app.config['SQLALCHEMY_DATABASE_URI'])
